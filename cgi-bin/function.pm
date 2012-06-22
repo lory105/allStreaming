ee64d22b2808fc0dc88db153307030939033d41a
Luca <luca@luca-Aspire-5610Z.(none)>
2012-06-22 17:25:03 +0200

    TOCCANDO FERRO, manca SOLO la relazione (aggiunto handheld) FINITO

--- cgi-bin/function.pm
+++ cgi-bin/function.pm
@@ -32,11 +32,6 @@ my $commentsXml = "../xml/comments.xml";
 sub header {
 	print "Content-type: text/html\n\n";
 	my $information = $_[1];
-	
-	#################################################################################################
-	# A COSA SERVE STO IF?? NON ENTRA MAI NELL'ELSE SOTTO XK NESSUN  ###################################
-	# CGI RICHIAMA HEADED PASSANDOGLI PARAMETRI !!!  ######################################################
-	################################################################################################
 	if ($information eq ""){
 	print <<HEADER;	
 	<?xml version="1.0" encoding="iso-8859-1"?>
@@ -50,7 +45,7 @@ sub header {
 			<link rel="icon" href="images/logo.ico" type="image/x-icon" />
 			<link rel="stylesheet" media="screen" href="../styles/baseStyle.css" type="text/css" />
 			<link rel="stylesheet" media="print" href="../styles/printStyle.css" type="text/css" />
-            <link rel="stylesheet" media="handheld screen and (max-width:480px), only screen and (max-device-width:480px)" href="../styles/smartStyle.css" type="text/css" />
+            <link rel="stylesheet" media="handheld" href="../styles/smartStyle.css" type="text/css" />
 			<script type="text/javascript" src="../javascript/validation.js"></script>
 		</head> 
 HEADER
